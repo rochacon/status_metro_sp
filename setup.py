bd5a98ce5be446e762c4a68d5673b9a01ba446bc
Rodrigo Chacon <rodrigo.chacon@titansgroup.com.br>
2014-04-10 22:37:37 -0300

    Added metrosp script

--- setup.py
+++ setup.py
@@ -13,4 +13,5 @@ setup(
     url = 'https://github.com/renatolipi/status_metro_sp',
     long_description = 'Ferramenta de linha de comando para recuperar situação atual das linhas do Metro de SP. Para mais infos: https://github.com/renatolipi/status_metro_sp/blob/master/README.md',
     install_requires = ['beautifulsoup4 >= 4.3.2', 'httplib2 == 0.7.4'],
+    scripts=['bin/metrosp'],
 )
